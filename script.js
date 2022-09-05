d251091c2b18cb9b46e37bc08438c9f691555730
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-09-05 23:07:53 +0200

    Update script.js
    Fix js linter errors

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const hamburger = document.querySelector('.bar');
 const navList = document.querySelector('.nav-list');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navList.classList.toggle('active');
-})
+  hamburger.classList.toggle('active');
+  navList.classList.toggle('active');
+});
 
 document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navList.classList.remove('active');
-  }));
+  hamburger.classList.remove('active');
+  navList.classList.remove('active');
+}));
