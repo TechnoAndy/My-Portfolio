20f7ae03244f08b80cc7c9bd67e63a2fc117f3e8
unknown <ekpenisiraphael@gmail.com>
2022-09-08 12:54:03 -0700

    email validated with javascript

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navList.classList.remove('active');
 }));
+
+function validate() {
+  const text = document.getElementById('text1').value;
+  const regx = /^([a-z0-9]+)@([a-z0-9-]+).([a-z]{2,20})(.[a-z]{2,8})?$/;
+
+  if (regx.test(text)) {
+    document.getElementById('ibitext').innerHTML = 'valid';
+    document.getElementById('ibitext').style.visibility = 'visible';
+    document.getElementById('ibitext').style.color = 'green';
+  } else {
+    document.getElementById('ibitext').innerHTML = 'invalid (lowercase only)';
+    document.getElementById('ibitext').style.visibility = 'visible';
+    document.getElementById('ibitext').style.color = 'red';
+  }
+}
+validate();
