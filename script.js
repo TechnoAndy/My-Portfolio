9b32be229235769b7e477c381ca879658174260e
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-09-08 20:00:49 +0200

    Update linter errors

--- script.js
+++ script.js
@@ -19,33 +19,29 @@ function fillInputs() {
   if (valueForName) {
     username.value = valueForName;
   }
-    username.addEventListener('change', function (event) {
+username.addEventListener('change', function (event) {
     username.value = event.target.value;
     localStorage.setItem('username', event.target.value);
   });
-
   const email = document.getElementById('text2');
   const ValueFormEmail = localStorage.getItem('email');
   if (ValueFormEmail) {
     email.value = ValueFormEmail;
   }
-
-    email.addEventListener('change', function (event) {
+email.addEventListener('change', function (event) {
     email.value = event.target.value;
     localStorage.setItem('email', event.target.value);
   });
-
   const text = document.getElementById('text3t');
   const valueForText = localStorage.getItem('text');
   if (valueForText) {
     text.value = valueForText;
   }
-    text.addEventListener('change', function (event) {
+text.addEventListener('change', function (event) {
     text.value = event.target.value;
     localStorage.setItem('text', event.target.value);
   });
 }
-
-  addEventListener('load', () => {
+addEventListener('load', () => {
     fillInputs();
 });
