561309b209a3e456bd0327a2cf5acde3b0a3effa
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-05-18 20:35:00 +0200

    Fix modal card view and adjust mobile view

--- script.js
+++ script.js
@@ -36,8 +36,10 @@ const cards = [
     id: 1,
     title: 'Meals DB',
     imageM: './images/Meals-DB.png',
-    imageD: './images/Meals-DB.png',
-    heading2: ['Meals', 'Front End Dev', '2022'],
+    imageD: '.images/Meals-DB.png',
+    heading2: ['Meals'],
+    heading3: ['Front End Dev'],
+    heading4: ['2022'],
     cardDescription:
       'This app is built using an API to access great recipes for people that love to cook. This is aimed at making it easier to store the recipes online and not have to keep a shelf full of recipe books.',
     languages: ['javascript', 'ES6', 'webpack'],
@@ -47,7 +49,9 @@ const cards = [
     title: 'Math Magicians',
     imageM: './images/Math-magicians.png',
     imageD: '.images/Math-magicians.png',
-    heading2: ['Calculator', 'Front End Dev', '2022'],
+    heading2: ['Calculator'],
+    heading3: ['Front End Dev'],
+    heading4: ['2022'],
     cardDescription:
       'This is a front-end mobile application that performs calculations for its users. This app allows individuals to enter mathematical equations and receive an immediate answer. It includes features such as basic arithmetic functions, scientific calculations, and a user-friendly interface for ease of use.',
     languages: ['html', 'css', 'react'],
@@ -57,7 +61,9 @@ const cards = [
     title: 'Seminar',
     imageM: './images/Seminar.png',
     imageD: '.images/Seminar.png',
-    heading2: ['Seminar', 'Front End Dev', '2022'],
+    heading2: ['Seminar'],
+    heading3: ['Front End Dev'],
+    heading4: ['2022'],
     cardDescription:
       'This is an app where a user can book a seat at a seminar, information about feature speakers and the program is available ',
     languages: ['html', 'css', 'javascript'],
@@ -67,17 +73,18 @@ const cards = [
     title: 'Rails Recipe App',
     imageM: './images/image4desktop.png',
     imageD: '.images/image4desktop.png',
-    heading2: ['Recipe', 'Back End Dev', '2023'],
+    heading2: ['Recipe'],
+    heading3: ['Back End Dev'],
+    heading4: ['2022'],
     cardDescription:
       'The Recipe app keeps track of all your recipes, ingredients, and inventory. It allows you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, since sharing recipes is an important part of cooking the app allows you to make them public so anyone can access them.',
     languages: ['ruby', 'postgreSQL', 'RSpec'],
   },
 ];
 
-
 const worksCard = cards.map(
-  (card, index) => `
-<section class="works ${index}">
+  (card, id) => `
+<section class="works ${id}">
 <div class="work-card">
 <img
   class="img"
@@ -85,11 +92,11 @@ const worksCard = cards.map(
   alt="SnapshootPortfolio02-img"
 />
 <div class="align-center-text">
-  <h3 class="work-heading ${index}">${card.title}</h3>
+  <h3 class="work-heading ${id}">${card.title}</h3>
   <ul class="dev-date">
-    <li class="canopy">CALCULATOR</li>
-    <li class="bullet">&#9679;&nbsp;&nbsp;&nbsp;Front End Dev</li>
-    <li class="bullet">&#9679;&nbsp;&nbsp;&nbsp;2022</li>
+    <li class="canopy">${card.heading2}</li>
+    <li class="bullet">&#9679;&nbsp;&nbsp;&nbsp;${card.heading3}</li>
+    <li class="bullet">&#9679;&nbsp;&nbsp;&nbsp;${card.heading4}</li>
   </ul>
   <p class="works-content-p">
   ${card.cardDescription}
@@ -100,14 +107,13 @@ const worksCard = cards.map(
     .join('')}
   </ul>
 
-  <button class="btn" data-card-id="${card.id}">See Project</button>
+  <button class="btn" data-card-id="${id}">See Project</button>
 
 </div>
 </div>
 </section>
 `
 );
-
 
 const workCard = document.querySelector('.works');
 workCard.innerHTML += worksCard;
@@ -116,9 +122,11 @@ const modalCard = [
   {
     id: 1,
     title: 'Meals DB',
-    imageM: './images/Recipe-App.png',
-    imageD: './images/Recipe-App.png',
-    heading2: ['Meals', 'Front End Dev', '2022'],
+    imageM: './images/Meals-DB.png',
+    imageD: './images/Meals-DB.png',
+    heading2: ['Meals'],
+    heading3: ['Front End Dev'],
+    heading4: ['2022'],
     cardDescriptionD:
       'This app is built using an API to access great recipes for people that love to cook. This is aimed at making it easier to store the recipes online and not have to keep a shelf full of recipe books.',
     cardDescriptionM:
@@ -133,7 +141,9 @@ const modalCard = [
     title: 'Math Magicians',
     imageM: './images/Math-magicians.png',
     imageD: './images/Math-magicians.png',
-    heading2: ['Calculator', 'Front End Dev', '2022'],
+    heading2: ['Calculator'],
+    heading3: ['Front End Dev'],
+    heading4: ['2022'],
     cardDescriptionD:
       'This is a front-end mobile application that performs calculations for its users. This app allows individuals to enter mathematical equations and receive an immediate answer. It includes features such as basic arithmetic functions, scientific calculations, and a user-friendly interface for ease of use.',
     cardDescriptionM:
@@ -148,7 +158,9 @@ const modalCard = [
     title: 'Seminar',
     imageM: './images/Seminar.png',
     imageD: './images/Seminar.png',
-    heading2: ['Seminar', 'Front End Dev', '2022'],
+    heading2: ['Seminar'],
+    heading3: ['Front End Dev'],
+    heading4: ['2022'],
     cardDescriptionD:
       'This is an app where a user can book a seat at a seminar, information about feature speakers and the program is available.',
     cardDescriptionM:
@@ -163,7 +175,9 @@ const modalCard = [
     title: 'Rails Recipe App',
     imageM: './images/Seminar.png',
     imageD: './images/Seminar.png',
-    heading2: ['Recipe', 'back End Dev', '2022'],
+    heading2: ['Recipe'],
+    heading3: ['Back End Dev'],
+    heading4: ['2022'],
     cardDescriptionD:
       'The Recipe app keeps track of all your recipes, ingredients, and inventory. It allows you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, since sharing recipes is an important part of cooking the app allows you to make them public so anyone can access them.',
     cardDescriptionM:
@@ -180,88 +194,77 @@ btns.forEach((btn) => {
   btn.addEventListener('click', () => {
     const cardId = btn.dataset.cardId;
     showPop(cardId);
-  });
-});
 
-function showPop(cardId) {
-  console.log("Button clicked! Card ID: " + cardId);
-}
-
-const modalCardDisplay = modalCard
-  .map(
-    (modeCard) => `
+    modalCard.forEach((item, index) => {
+      if (index == cardId) {
+        const modalCards = document.getElementById('modal-cards');
+        console.log(modalCards);
+        const content = `
 
 <div class="card-portfolio-1">
 
-            <li class="pop-close"><i class="fa fa-times"></i></li>
-                <h2 class="work-heading">${modeCard.title}</h2>
-                <ul class="dev-date space-date">
-                <li class="canopy">MEALS</li>
-                <li class="bullet">&#9679;&nbsp;&nbsp;&nbsp;Front End Dev</li>
-                <li class="bullet">&#9679;&nbsp;&nbsp;&nbsp;2022</li>
-              </ul>
-              <div class="images">
-         <img class="forD-1" src="${modeCard.imageD}" alt="Pop Up Desktop" >
-      <img class="forM-1" src="${modeCard.imageM}" alt="Pop Up Mobile" >
+<li class="pop-close"><i class="fa fa-times"></i></li>
+    <h2 class="work-heading">${item.title}</h2>
+    <ul class="dev-date space-date">
+    <li class="canopy">${item.heading2}</li>
+    <li class="bullet">&#9679;&nbsp;&nbsp;&nbsp;${item.heading3}</li>
+    <li class="bullet">&#9679;&nbsp;&nbsp;&nbsp;${item.heading4}</li>
+  </ul>
+  <div class="images">
+<img class="forD-1" src="${item.imageD}" alt="Pop Up Desktop" >
+<img class="forM-1" src="${item.imageM}" alt="Pop Up Mobile" >
 
-         </div>
-      <div>
-      <div class="row">
-      <div class="column-left-1">
-          <p class="forD-1">
-              ${modeCard.cardDescriptionM}
-          </p>
-          <p class="forM-1">
-              ${modeCard.cardDescriptionM}
-          </p>
+</div>
+<div>
+<div class="row">
+<div class="column-left-1">
+<p class="forD-1">
+  ${item.cardDescriptionM}
+</p>
+<p class="forM-1">
+  ${item.cardDescriptionD}
+</p>
 
-      </div>
-      <div class="column-right-1">
-          <ul class="modal-1">
-              ${modeCard.languagesM
-                .map((lang) => `<li class="works-badge">${lang}</li>`)
-                .join('')}
-          </ul>
-          <ul id="miss" class="miss modal-1">
-          ${modeCard.languagesD
-            .map((lang) => `<li class="works-badge">${lang}</li>`)
-            .join('')}
-          </ul>
-          <hr>
-          <ul class="modal-1">
-              <li class="modal-3" href="${
-                modeCard.linkLive
-              }">See live <img class="btnimag" src="./images/seeliveIcon.png"></li>
-              <li class="modal-3" href="${
-                modeCard.linkSource
-              }">See Source <i class="fa fa-github"></i></li>
-          </ul>
-      </div>
-  </div>
-      </div>
-  </div>
-`
-  )
-  .join('');
+</div>
+<div class="column-right-1">
+<ul class="modal-1">
+  ${item.languagesM.map((lang) => `<li class="works-badge">${lang}</li>`)}
+</ul>
 
-const modalCards = document.getElementById('modal-cards');
-modalCards.innerHTML += modalCardDisplay;
-const boxModal = document.querySelector('.card-portfolio-1');
-const openModal = document.querySelectorAll('.btn');
+<hr>
+<ul class="modal-1">
+  <li class="modal-3" href="${
+    item.linkLive
+  }">See live <img class="btnimag" src="./images/seeliveIcon.png"></li>
+  <li class="modal-3" href="${
+    item.linkSource
+  }">See Source <i class="fa fa-github"></i></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+       `;
+        modalCards.innerHTML = content;
+        const boxModal = document.querySelector('.card-portfolio-1');
+        const openModal = document.querySelectorAll('.btn');
 
-openModal.forEach((open) => {
-  open.addEventListener('click', () => {
-    boxModal.style.display = 'block';
-    disableScroll();
+        boxModal.style.display = 'block';
+
+        const popClose = document.querySelector('.pop-close');
+        popClose.addEventListener('click', () => {
+          boxModal.style.display = 'none';
+          //    body.style.filter = 'none';
+          //    enableScroll();
+        });
+      }
+    });
   });
 });
 
-const popClose = document.querySelector('.pop-close');
-popClose.addEventListener('click', () => {
-  boxModal.style.display = 'none';
-  body.style.filter = 'none';
-  enableScroll();
-});
+function showPop(id) {
+  /* console.log('Button clicked! Card ID: ' + id); */
+}
 
 // languages close toggle
 
